bbfe29f0560f6088eeb2ea3cf04728d67e272267
SarahHessel <sarah.phessel@gmail.com>
2020-11-24 13:18:28 -0300

    Erro de repositorio consertado

--- pokedex/src/pages/DetailsPage/index.js
+++ pokedex/src/pages/DetailsPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../components/Header/index';
+import Header from '../../components/header/index';
 import { DetailsContainer, ImagesContainer, StatsContainer, TypesMovesContainer } from './styled';
 
 const Details = () => {
--- pokedex/src/pages/PokedexPage/index.js
+++ pokedex/src/pages/PokedexPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../components/Header';
+import Header from '../../components/header/index';
 import PokeCard from '../../components/PokemonCard';
 import { CardsContainer } from './styled';
 
